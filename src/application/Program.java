d86ffe81c264c0e45b9a7b227094b0d8a3c2b338
Darlan Rodrigues <darlanrodriguesp97@hotmail.com>
2020-07-16 13:21:07 -0300

    Criação da classe Funcionario

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Livros");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Darlan", "darlan@gmail.com", new Date(), 3000.00, obj);
+		
+		System.out.println(seller);
 	}
 }
